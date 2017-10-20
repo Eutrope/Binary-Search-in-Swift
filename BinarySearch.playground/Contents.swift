f3742b18126d8059a7f76cc050d34a6753dec202
Ha Mi Nguyen <haminguyen@hotmail.com>
2017-10-20 04:47:37 -0400

    Add files via upload

---
+++ BinarySearch.playground/Contents.swift
@@ -0,0 +1,43 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+let arr = [1,2,3,4,5,6]
+
+// Linear search is O(n) since we go through the array
+func linearSearch(searchValue: Int, array: [Int]) -> Bool {
+    for num in array {
+        if num == searchValue {
+            return true
+        }
+    }
+    return false
+}
+
+// BinarySearch performs in O(log(n)) since we are going through half of the array for each iteration of the loop
+func binarySearch(searchValue: Int, array: [Int]) -> Bool{
+    var leftIndex = 0
+    var rightIndex = array.count - 1
+    
+    while leftIndex <= rightIndex {
+        let middleIndex = (leftIndex + rightIndex) / 2
+        let middleValue = array[middleIndex]
+        
+        if middleValue == searchValue {
+            return true
+        }
+		
+		// Ignore right half is x is smaller
+        if searchValue < middleValue {
+            rightIndex = middleIndex - 1
+        }
+			
+		// Ignore left half is x is greater
+        else {
+            leftIndex = middleIndex + 1
+        }
+    }
+    return false
+}
+
+print(linearSearch(searchValue: 4, array: arr))
+print(binarySearch(searchValue: 1, array: arr))
